6bfbf105b35650a4df96068f33695e53378d125c
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-02-15 22:33:17 +0100

    ✨ project card component in progress

---
+++ src/components/molecules/projectcard/ProjectCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-card">
+    <div class="project-card-header">
+      <img :src="project.image" alt="Project Image" />
+      <div class="project-card-title">{{ project.title }}</div>
+    </div>
+    <div class="project-card-body">
+      <div class="project-card-description">{{ project.description }}</div>
+      <div class="project-card-tags">
+        <span v-for="tag in project.tags" :key="tag" class="project-card-tag">{{ tag }}</span>
+      </div>
+    </div>
+    <div class="project-card-footer">
+      <a :href="project.link" target="_blank" rel="noopener noreferrer" class="project-card-link">View Project</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+  
+<style scoped lang="scss">
+@import '_projectcard.scss'
+</style>
+  
--- src/components/organisms/projects/Projects.vue
+++ src/components/organisms/projects/Projects.vue
@@ -1,9 +1,52 @@
-<script setup>
-</script>
-
 <template>
-    <div>Projects</div>
-</template>
+    <div>
+      <h1>Projects</h1>
+        <div class="project__cards">
+            <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  import ProjectCard from '../../molecules/projectcard/ProjectCard.vue';
+  
+  export default {
+    name: 'Projects',
+    components: {
+      ProjectCard
+    },
+    data() {
+      return {
+        projects: [
+          {
+            id: 1,
+            title: "Vue.js Project",
+            description: "This is a project built with Vue.js that does something cool.",
+            image: "https://source.unsplash.com/300x200/?random",
+            tags: ["Vue.js", "JavaScript", "Web Development"],
+            link: "https://github.com/your-username/vue-project"
+          },
+          {
+            id: 2,
+            title: "React Project",
+            description: "This is a project built with React that does something awesome.",
+            image: "https://source.unsplash.com/300x200/?random",
+            tags: ["React", "JavaScript", "Web Development"],
+            link: "https://github.com/your-username/react-project"
+          },
+          {
+            id: 3,
+            title: "Coooool Project",
+            description: "This is a project built with GHATGPT that does something awesome.",
+            image: "https://source.unsplash.com/300x200/?random",
+            tags: ["React", "JavaScript", "Web Development", "CHATGPT"],
+            link: "https://github.com/your-username/react-project"
+          }
+        ]
+      }
+    }
+  }
+  </script>
 
 <style scoped lang="scss">
 @import '_projects.scss'
