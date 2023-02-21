afae92f0079eee1de8c5a602519a87c97fa85902
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-02-21 16:00:38 +0100

    ✨add Contact section

--- src/components/organisms/contact/Contact.vue
+++ src/components/organisms/contact/Contact.vue
@@ -2,7 +2,30 @@
 </script>
 
 <template>
-    <div>Contact</div>
+	<section id="contact">
+		<div class="contact__container">
+			<h2 class="contact__h2">
+				Contact!
+			</h2>
+			<div class="contact__text__container">
+				<p class="contact__text">
+				Thank you for checking out my portfolio.
+				</p>
+				<p class="contact__text">
+					You can reach me on both linkedIn, mail and phone. Let us Talk!
+				</p>
+			</div>
+			<div class="contact__icons">
+				<a href="https://www.linkedin.com/in/davidstalgren/" target="_blank"><img src="src/assets/icons/icons8-linkedin-100.svg" alt="linkedIn icon" width="50" height="50"></a>
+        <a href="https://github.com/davidstalgren" target="_blank"><img src="src/assets/icons/icons8-github-100.svg" alt="Github icon" width="50" height="50"></a>
+				<a href="mailto:david.stalgren@medieinstitutet.se?subject=Let us Talk!" target="_blank"><img src="src/assets/icons/icons8-envelope-100.png" alt="Mail icon" width="50" height="50"></a>
+			</div>
+			<div class="contact__links">
+				<span class="contact__link"><a href="mailto:david.stalgren@medieinstitutet.se?subject=Let us Talk!" target="_blank">david.stalgren@medieinstitutet.se</a></span><br>
+				<span class="contact__link"><a href="tel:+46738392710" target="_blank">0738392710</a></span>
+			</div>
+		</div>
+	</section>
 </template>
 
 <style scoped lang="scss">
